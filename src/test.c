5368a0576c73d8333e51288a4f9e04bbbf233543
august <theonlyaugust@gmail.com>
2024-11-07 00:53:51 -0500

    made "tests" folder, to hold test .exe files

--- src/test.c
+++ src/test.c
@@ -8,11 +8,17 @@ int main() {
 
 	char filepath[256], filepath2[256];
 	struct wav_data data;
+	float m;
 
-	printf("this is a test of the wav_write() function.\n");
+	printf("this program modifies the sample rate of a .wav file.\n");
 	printf("path to .wav file: ");
 	scanf("%s", filepath);
 	data = wav_read(filepath);
+
+	// mess with stuff
+	printf("sample rate multiplier: ");
+	scanf("%f", &m);
+	data.sample_rate *= m;
 	
 	printf("output file path: ");
 	scanf("%s", filepath2);
